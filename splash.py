01bda5151d6eb1a79c6fcd16a368c1e68d19c0d1
Kharlo109 <kharlopena@gmail.com>
2021-10-10 21:31:01 -0400

    Added button interface and new bg image.

--- splash.py
+++ splash.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 
 def title_screen():
-
-    global bg
+    # Turn background to global to avoid Python garbage collector
+    global background
 
     # Create object
     splash_root = Tk()
@@ -14,11 +14,26 @@ def title_screen():
     # Top bar label
     splash_root.title("Dungeons & Wagons")
 
+    # Background image definition
+    background = PhotoImage(file="images/splash_screen.png")
+
+    # Canvas overlay object
+    splash_canvas = Canvas(splash_root, width=1280, height=720)
+    splash_canvas.pack(fill="both", expand=True)
+
+    # Background image placement on canvas
+    splash_canvas.create_image(0, 0, image=background, anchor="nw")
+
     # Title
 
-    bg = PhotoImage(file="images/1.png")
-    my_picture = Label(splash_root, image=bg)
-    my_picture.place(x=0, y=0, relwidth=1, relheight=1)
+    splash_canvas.create_text(640, 50, text="Dungeons & Wagons", font=("Courier New", 40), fill="white")
+
+    # Menu buttons
+    exit_button = Button(splash_root, text="EXIT", font=("Courier New", 10))
+    play_button = Button(splash_root, text="PLAY", font=("Courier New", 10))
+
+    exit_button_window = splash_canvas.create_window(10, 10, anchor="nw", window=exit_button)
+    play_button_window = splash_canvas.create_window(65, 10, anchor="nw", window=play_button)
 
 
 title_screen()
